1bc5df734585a8e1ffff2e87bdf20750425ced0e
efty <efty3222@gmail.com>
2024-10-15 19:53:51 +0600

    initial commit

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,11 +5,10 @@ const Hero = () => {
     <div className=' py-5'>
       <Container className='d-flex justify-content-center'>
         <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
-          <h1 className='text-center mb-4'>MERN Authentication</h1>
+          <h1 className='text-center mb-4'>Stack Overflow</h1>
           <p className='text-center mb-4'>
-            This is a boilerplate for MERN authentication that stores a JWT in
-            an HTTP-Only cookie. It also uses Redux Toolkit and the React
-            Bootstrap library
+          Stack Overflow is a popular online platform where developers can ask and answer coding-related questions. 
+          It serves as a collaborative space for sharing programming knowledge and solutions.
           </p>
           <div className='d-flex'>
             <Button variant='primary' href='/login' className='me-3'>
